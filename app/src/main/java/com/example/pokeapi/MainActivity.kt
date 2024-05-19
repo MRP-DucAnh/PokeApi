d595d3a452b40a1f80553b43dacfc552407e5b02
Pear <leducanh51501@gmail.com>
2024-05-19 15:56:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pokeapi/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.pokeapi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+git config --global user.email "leducanh51501@gmail.com"
+git config --global user.name "Pear"
